0d0f33ea31782731bd36f1b9d04044cf64fe4501
MohamedM7 <mammeri244@gmail.com>
2023-11-07 03:12:17 +0100

    Updating & Testing Payments

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
+/*--  Mammeri Moahmed --------------------------------------------------
+ *    ==================================================================
+ *    Website : www.mohamedmammeri.com
+ *    Email   : mammeri244@gmail.com
+ * --------------------------------------------------------------------*/
 
-    // Loop over each payment method
-    paypal.getFundingSources().forEach(function (fundingSource) {
-        // Initialize the buttons
-        var button = paypal.Buttons({
-          fundingSource: fundingSource,
-        })
-        // Check if the button is eligible
-        if (button.isEligible()) {
-        // Render the standalone button for that payment method
-          button.render('#PaypalSection')
-        }
-      })
+
+paypal.Buttons({
+    createOrder: function(data, actions) {
+        return actions.order.create({
+            purchase_units: [{
+                amount: {
+                    value: '10.00' // Set the amount you want to charge or pass it within a var
+                }
+            }]
+        });
+    },
+    onApprove: function(data, actions) {
+        return actions.order.capture().then(function(details) {
+            // Handle the successful payment here
+            console.log('Payment completed successfully: ' + details.payer.name.given_name);
+        });
+    }
+}).render('#paypal-button-container');
+
+
+/* paypal.Buttons({   // Customize your buttons by passing in the style option.
+    style: {
+      layout: 'vertical',//
+      color:  'blue',
+      shape:  'rect',
+      label:  'paypal'
+    }
+  }).render('#paypal-button-container');
+      */
     
